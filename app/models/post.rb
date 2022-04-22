8334a3c216567902256bf023e8c9bd85be26261f
rodrigoraiman <rodrigoraiman@gmail.com>
2022-04-22 11:05:44 -0400

    posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
     validates :tittle, presence: true, length: {minimum: 5, maximum: 50}
     validates :body, presence: true, length: {minimum: 5, maximum: 50}
+    belongs_to :user
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(email: "user@example.com", password: "password", password_confirmation: "password")
+10.times do |x|
+    Post.create(tittle: "Title #{x}", body: "body #{x} ahora si", user_id: User.first.id)
+end
