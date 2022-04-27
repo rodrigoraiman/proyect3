3a0997080579d54a7ca3e607d431f6710f7e1ce8
rodrigoraiman <rodrigoraiman@gmail.com>
2022-04-27 12:20:32 -0400

    controller

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-    validates :tittle, presence: true, length: {minimum: 5, maximum: 50}
-    validates :body, presence: true, length: {minimum: 5, maximum: 50}
+    validates :tittle, presence: true, length: { minimum: 5, maximum: 50 }
+    validates :body, presence: true, length: { minimum: 5, maximum: 50 }
     belongs_to :user
 end
